b539cd95251332c1a01daf667bf850a6a951485a
ksan2525 <66823857+ksan2525@users.noreply.github.com>
2020-08-09 18:04:57 +0900

    プレイヤーの移動ができた、視点を動かそうとしたけれどバグった

---
+++ Assets/script/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject mainCamera;
+    public float rotate_speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCamera = Camera.main.gameObject;
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = player.transform.position;
+        Move();
+        Camera_control();
+    }
+
+    void Move()
+    {
+        float dx = Input.GetAxis("Horizontal");
+        float dz = Input.GetAxis("Vertical");
+        transform.Translate(dx, 0.0f, dz);
+    }
+
+    void Camera_control()
+    {
+        Vector3 angle = new Vector3(
+            Input.GetAxis("Mouse X") * rotate_speed,
+            Input.GetAxis("Mouse Y") * rotate_speed,
+            0
+        );
+
+        transform.RotateAround(mainCamera.transform.position, Vector3.up, angle.x);
+        transform.RotateAround(player.transform.position, transform.right, angle.y);
+    }
+}
