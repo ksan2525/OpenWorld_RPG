145e0e9d3c0cf2bebf52cfe0210aa076a2658e11
ksan2525 <66823857+ksan2525@users.noreply.github.com>
2020-08-26 20:59:59 +0900

    コントローラーで視点移動をしようとしたけれどできなかった

--- Assets/script/Player.cs
+++ Assets/script/Player.cs
@@ -23,11 +23,14 @@ public class Player : MonoBehaviour
     }
 
     // Update is called once per frame
+    float sight_x = 0;
+    float sight_y = 0;
     void Update()
     {
         transform.position = player.transform.position;
         Move();
         Camera_control();
+        Camera_joystick();
     }
 
     void OnCollisionStay(Collision col)
@@ -52,5 +55,44 @@ public class Player : MonoBehaviour
         float yRotation = Input.GetAxis("Mouse Y") * YSpeed;
         YAxis.transform.Rotate(0, xRotation, 0);
         XAxis.transform.Rotate(-yRotation, 0, 0);
+    }
+    void Camera_joystick()
+    {
+    //    float angleH = Input.GetAxis("joystick X") * 5.0f;
+    //    float angleV = Input.GetAxis("joystick Y") * 5.0f;
+    //    YAxis.transform.Rotate(0, angleH, 0);
+    //    XAxis.transform.Rotate(-angleV, 0, 0);
+        
+        //if (sight_y > 80)
+        //{
+        //    if(angleV < 0)
+        //    {
+        //        sight_y = sight_y + angleV;
+        //    }
+        //}
+        //else if(sight_y < -90)
+        //{
+        //    if(angleV > 0)
+        //    {
+        //        sight_y = sight_y + angleV;
+        //    }
+        //}
+        //else
+        //{
+        //    sight_y = sight_y + angleV;
+        //}
+
+        //if(sight_x >= 360)
+        //{
+        //    sight_x = sight_x - 360;
+        //}
+        //else if(sight_x < 0)
+        //{
+        //    sight_x = 360 - sight_x;
+        //}
+        //sight_x = sight_x + angleH;
+        //transform.localRotation = Quaternion.Euler(sight_y, sight_x, 0);
+
+
     }
 }
