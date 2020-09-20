11dc04e5193381f9bc034876662bda617fb2e96e
ksan2525 <66823857+ksan2525@users.noreply.github.com>
2020-09-20 18:03:57 +0900

    hpを作り始めた

--- Assets/script/Parameter.cs
+++ Assets/script/Parameter.cs
@@ -4,24 +4,53 @@ using UnityEngine;
 
 public class Parameter : MonoBehaviour
 {
+    public int lv;
+    public int xp;
+    public int hp;
+    private float timeleft;
     // Start is called before the first frame update
     void Start()
     {
+        lv = 1;
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        LV();
     }
 
     void HP()
     {
         
+        if (lv == 1)
+        {
+            hp = 100;
+        }
+        if (lv == 2)
+        {
+            hp = 110;
+        }
+        
     }
     void MP()
     {
 
+    }   
+    void LV()
+    {
+        if (xp == 10)
+        {
+            lv = 2;
+        }
+    }
+    void XP()
+    {
+        if(timeleft <= 0.0)
+        {
+            timeleft = 1.0f;
+            xp + 1;
+        }
     }
 }
---
+++ Assets/script/ParameterText.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ParameterText : MonoBehaviour
+{
+    GameObject player;
+    Parameter parameter;
+    [SerializeField] Text lvtext;
+    public int lv;
+    public int xp;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        parameter = player.GetComponent<Parameter>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xp = player.GetComponent<Parameter>().xp;
+        lv = player.GetComponent<Parameter>().lv;
+        lvtext.text = "LV=" + lv;
+    }
+}
